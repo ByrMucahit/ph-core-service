445ff36cf906d61046b9bb9258df7d94e47da724
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 18:02:15 +0300

    feat: "add money-transaction.data-access.ts to handle data from database"

---
+++ src/money-transaction/money-transaction.data-access.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MoneyTransactionEntity } from './money-transaction.entity';
+import { Repository } from 'typeorm';
+import { CreateTransactionDto } from './dtos/create-transaction.dto';
+
+@Injectable()
+export class MoneyTransactionDataAccess {
+  constructor(
+    @InjectRepository(MoneyTransactionEntity)
+    private moneyTransactionRepository: Repository<MoneyTransactionEntity>,
+  ) {}
+
+  async createMoneyTransaction(createTransaction: CreateTransactionDto) {
+    const moneyTransactionEntity = await this.moneyTransactionRepository.create(createTransaction);
+    moneyTransactionEntity.created_at = new Date();
+    moneyTransactionEntity.updated_at = new Date();
+    await this.moneyTransactionRepository.save(moneyTransactionEntity);
+  }
+}
