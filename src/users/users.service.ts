040af0fa61d80cb00e77a4e9d25819423caca150
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 18:06:13 +0300

    feat: "update user.service.ts to add new methods to organize the user to make money"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,16 @@ import { UsersDataAccess } from './users.data-access';
 import { CreateUsersDto } from './dtos/create-users.dto';
 import { Injectable } from '@nestjs/common';
 import { USERS_ENTITY } from '../constants/users.constants';
+import { CreateMoneyTransactionDto } from '../money-transaction/dtos/create-money-transaction.dto';
+import { UserNotFoundExceptions } from './exceptions/user-not-found.exceptions';
+import { MoneyTransactionService } from '../money-transaction/money-transaction.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private usersDataAccess: UsersDataAccess) {}
+  constructor(
+    private usersDataAccess: UsersDataAccess,
+    private moneyTransactionService: MoneyTransactionService,
+  ) {}
 
   private decorateProjectionObject(projection?: string) {
     const currentProjection = {};
@@ -26,5 +32,15 @@ export class UsersService {
     const decoratedProjection: { [key: string]: 1 } =
       typeof projection === 'string' ? this.decorateProjectionObject(projection) : projection;
     return this.usersDataAccess.findUsers(decoratedProjection);
+  }
+
+  async makeMoney(createMoneyTransaction: CreateMoneyTransactionDto, userId: string) {
+    const user = await this.usersDataAccess.findUserById(userId, { id: 1 });
+    if (!user) throw new UserNotFoundExceptions();
+    const createTransactionDto: any = {
+      user_id: userId,
+      amount: createMoneyTransaction.amount,
+    };
+    await this.moneyTransactionService.createMoneyTransaction(createTransactionDto);
   }
 }
