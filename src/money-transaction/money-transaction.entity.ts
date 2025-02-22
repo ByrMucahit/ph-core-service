6e8cde7825edf14b292917963a3e5f7f4315cd7e
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 18:02:57 +0300

    feat: "add money-transaction.entity.ts to map data fro money-transaction database"

---
+++ src/money-transaction/money-transaction.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'money-transaction' })
+export class MoneyTransactionEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column('decimal', { precision: 6, scale: 2 })
+  amount: number;
+
+  @Column('decimal', { precision: 6, scale: 2 })
+  amount_to_pool: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column({ nullable: false })
+  updated_at!: Date;
+}
