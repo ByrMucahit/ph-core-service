6668927c9262f234c5eb119f9e3386def097a60b
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 14:19:43 +0300

    feat: "update typeorm.module.ts to refine the way of taking credentials"

--- src/datasource/typeorm.module.ts
+++ src/datasource/typeorm.module.ts
@@ -1,35 +1,36 @@
 import { Global, Module } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-
-
+import getEnv from '../helpers/get-env';
 
 @Global()
 @Module({
   imports: [],
-  providers: [{
-    provide: DataSource,
-    inject: [],
-    useFactory: async () => {
-      try {
-        const dataSource = new DataSource({
-          type: 'postgres',
-          host: 'localhost',
-          port: 5432,
-          username: 'myuser',
-          password: 'mypassword',
-          database: 'mydatabase',
-          synchronize: true,
-          entities: [__dirname+ '/dist/../../**/**.entity{.ts,.js}'],
-        });
-        await dataSource.initialize();
-        console.log('Database connected successfully');
-        return dataSource;
-      } catch (error) {
-        console.log('Error connecting to database');
-        throw error;
-      }
-    }
-  }],
-  exports: [DataSource]
+  providers: [
+    {
+      provide: DataSource,
+      inject: [],
+      useFactory: async () => {
+        try {
+          const dataSource = new DataSource({
+            type: 'postgres',
+            host: getEnv('HOST'),
+            port: Number(getEnv('DB_PORT')),
+            username: getEnv('USER_NAME'),
+            password: String(getEnv('PASSWORD')),
+            database: getEnv('DB_NAME'),
+            synchronize: true,
+            entities: [__dirname + '/dist/../../**/**.entity{.ts,.js}'],
+          });
+          await dataSource.initialize();
+          console.log('Database connected successfully');
+          return dataSource;
+        } catch (error) {
+          console.log('Error connecting to database');
+          throw error;
+        }
+      },
+    },
+  ],
+  exports: [DataSource],
 })
 export class DataSourceModule {}
