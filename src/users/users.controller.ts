128f06cc808bb0b6f19d8bc01c79ae9798c2f0f4
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 14:20:18 +0300

    feat: "update users.controller.ts to add new controller to handle request to create user"

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,13 +3,12 @@ import { UsersService } from './users.service';
 import { CreateUsersDto } from './dtos/create-users.dto';
 
 @Controller('users')
-export class UsersController{
-  constructor(private usersService: UsersService) {
-  }
+export class UsersController {
+  constructor(private usersService: UsersService) {}
 
   @Post('create')
   @HttpCode(HttpStatus.OK)
   async createUser(@Body() createUserDto: CreateUsersDto) {
-
+    return this.usersService.createUser(createUserDto);
   }
 }
