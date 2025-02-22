97905b2820013b0f16962489f14759c6de4774a6
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 14:23:30 +0300

    feat: "update users.data-access.ts to create user"

---
+++ src/users/users.data-access.ts
@@ -0,0 +1,20 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersEntity } from './users.entity';
+import { Repository } from 'typeorm';
+import { CreateUsersDto } from './dtos/create-users.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersDataAccess {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  async createUser(createUsersDto: CreateUsersDto) {
+    const usersEntity = this.usersRepository.create(createUsersDto);
+    usersEntity.created_at = new Date();
+    usersEntity.updated_at = new Date();
+    return this.usersRepository.save(usersEntity);
+  }
+}
