bcaa13642f4171248a98f70f74f7eaeff36afba8
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 17:59:18 +0300

    feat: "update app.module.ts to add new module for money-transaction"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DataSourceModule } from './datasource/typeorm.module';
 import { UsersModule } from './users/users.module';
 import { AppGateway } from './gateway/app-gateway';
+import { MoneyTransactionModule } from './money-transaction/money-transaction.module';
 
 @Module({
-  imports: [DataSourceModule, UsersModule],
+  imports: [DataSourceModule, UsersModule, MoneyTransactionModule],
   controllers: [],
   providers: [AppGateway],
 })
