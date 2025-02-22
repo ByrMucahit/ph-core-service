d773b6de4690627a49a78099988fef3128f053a1
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 14:20:41 +0300

    feat: "update users.service.ts to add new method to organize the data to create"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,12 @@
-export class UsersService{}
+import { UsersDataAccess } from './users.data-access';
+import { CreateUsersDto } from './dtos/create-users.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  constructor(private usersDataAccess: UsersDataAccess) {}
+
+  async createUser(createUsersDto: CreateUsersDto) {
+    await this.usersDataAccess.createUser(createUsersDto);
+  }
+}
