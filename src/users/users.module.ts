6e33f53b1e7c371ed9d2311c8e746c544702fb13
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 18:05:40 +0300

    feat: "update users.module.ts to add new dependencies into user module"

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersEntity } from './users.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersDataAccess } from './users.data-access';
+import { MoneyTransactionModule } from '../money-transaction/money-transaction.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersEntity])],
+  imports: [TypeOrmModule.forFeature([UsersEntity]), MoneyTransactionModule],
   providers: [UsersService, UsersDataAccess],
   controllers: [UsersController],
   exports: [UsersService],
