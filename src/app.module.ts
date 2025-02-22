27577a70c3b976b4603e93171d57fbd8fcaade67
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 16:30:29 +0300

    feat: "update app.module.ts to add new dependencies in the module"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DataSourceModule } from './datasource/typeorm.module';
 import { UsersModule } from './users/users.module';
+import { AppGateway } from './gateway/app-gateway';
 
 @Module({
   imports: [DataSourceModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [AppGateway],
 })
 export class AppModule {}
