8c2747b288dbbd1753c8568df4dd1fb032911c99
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 18:00:25 +0300

    feat: "add money-transaction.service.ts to organize CRUD transactions for money-transaction"

---
+++ src/money-transaction/money-transaction.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { MoneyTransactionDataAccess } from './money-transaction.data-access';
+import { CreateTransactionDto } from './dtos/create-transaction.dto';
+
+@Injectable()
+export class MoneyTransactionService {
+  RATE_FOR_POOL = 0.2;
+  constructor(private moneyTransactionDataAccess: MoneyTransactionDataAccess) {}
+
+  async createMoneyTransaction(createTransactionDto: CreateTransactionDto) {
+    const distributeMoney = this.distributeMoneyIntoPoolAndOwner(createTransactionDto.amount);
+    createTransactionDto.amount = distributeMoney.amountForOwner;
+    createTransactionDto.amount_to_pool = distributeMoney.amountForPool;
+    createTransactionDto.created_at = new Date();
+    createTransactionDto.update_at = new Date();
+    await this.moneyTransactionDataAccess.createMoneyTransaction(createTransactionDto);
+  }
+
+  private distributeMoneyIntoPoolAndOwner(amount: number) {
+    const amountForPool = amount * this.RATE_FOR_POOL;
+    return { amountForPool, amountForOwner: amount - amountForPool };
+  }
+}
