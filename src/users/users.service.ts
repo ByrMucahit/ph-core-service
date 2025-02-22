a56431a2853d0707b8003cea2bccc844f65b0fe3
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 15:00:47 +0300

    feat: "update users.service.ts to add new decorator to make request fit to select something from db and add new method to organize data to respond"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,30 @@
 import { UsersDataAccess } from './users.data-access';
 import { CreateUsersDto } from './dtos/create-users.dto';
 import { Injectable } from '@nestjs/common';
+import { USERS_ENTITY } from '../constants/users.constants';
 
 @Injectable()
 export class UsersService {
   constructor(private usersDataAccess: UsersDataAccess) {}
 
+  private decorateProjectionObject(projection?: string) {
+    const currentProjection = {};
+    if (projection) {
+      const array: string[] = projection.split(' ');
+      array.forEach((element) => {
+        if (USERS_ENTITY.has(element)) currentProjection[element] = 1;
+      });
+    }
+    return currentProjection;
+  }
+
   async createUser(createUsersDto: CreateUsersDto) {
     await this.usersDataAccess.createUser(createUsersDto);
+  }
+
+  async findUsers(projection: string | { [key: string]: 1 }) {
+    const decoratedProjection: { [key: string]: 1 } =
+      typeof projection === 'string' ? this.decorateProjectionObject(projection) : projection;
+    return this.usersDataAccess.findUsers(decoratedProjection);
   }
 }
