132f9237bf42eca9656050764c704a41c9d203ce
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-22 18:05:03 +0300

    feat: "update users.controller.ts to add new endpoint to make money by user"

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUsersDto } from './dtos/create-users.dto';
+import { UUIDParam } from '../decorators/http.decorators';
+import { CreateMoneyTransactionDto } from '../money-transaction/dtos/create-money-transaction.dto';
 
 @Controller('users')
 export class UsersController {
@@ -16,5 +18,14 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   async getUsers(@Query('projection') projection: string) {
     return this.usersService.findUsers(projection);
+  }
+
+  @Post('/:user_id/money')
+  @HttpCode(HttpStatus.OK)
+  async makeMoney(
+    @UUIDParam('user_id') userId: Uuid,
+    @Body() createMoneyTransactionDto: CreateMoneyTransactionDto,
+  ) {
+    await this.usersService.makeMoney(createMoneyTransactionDto, userId);
   }
 }
