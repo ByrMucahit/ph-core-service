6f121879f11e019a0940b7770f59f32a5282d23d
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-25 21:17:56 +0300

    feat: "update app.module.ts to add new module which is named user-profile"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './users/users.module';
 import { AppGateway } from './gateway/app-gateway';
 import { MoneyTransactionModule } from './money-transaction/money-transaction.module';
 import { RedisModule } from './redis/redis.module';
+import { UserProfileModule } from './user-profile/user-profile.module';
 
 @Module({
-  imports: [DataSourceModule, UsersModule, MoneyTransactionModule, RedisModule],
+  imports: [DataSourceModule, UsersModule, MoneyTransactionModule, RedisModule, UserProfileModule],
   controllers: [],
   providers: [AppGateway],
 })
