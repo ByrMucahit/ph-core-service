3cd193b31e2532cc76b64d95818d625f61e59310
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-25 20:01:35 +0300

    feat: "update app.module.ts to add new moduşe"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DataSourceModule } from './datasource/typeorm.module';
 import { UsersModule } from './users/users.module';
 import { AppGateway } from './gateway/app-gateway';
 import { MoneyTransactionModule } from './money-transaction/money-transaction.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [DataSourceModule, UsersModule, MoneyTransactionModule],
+  imports: [DataSourceModule, UsersModule, MoneyTransactionModule, RedisModule],
   controllers: [],
   providers: [AppGateway],
 })
