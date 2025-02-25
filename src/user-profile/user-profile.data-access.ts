e31f9345e423feb6824a29b91e3dbccb893f1d56
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-25 21:51:23 +0300

    feat: "update user-profile.data-access.ts to add UPDATE transaction"

--- src/user-profile/user-profile.data-access.ts
+++ src/user-profile/user-profile.data-access.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserProfileEntity } from './user-profile.entity';
 import { Repository } from 'typeorm';
 import { CreateUserProfileDto } from './dtos/create-user-profile.dto';
+import { UpdateUserProfileDto } from './exceptions/update-user-profile.dto';
 
 @Injectable()
 export class UserProfileDataAccess {
@@ -22,5 +23,18 @@ export class UserProfileDataAccess {
     userProfileEntity.created_at = new Date();
     userProfileEntity.updated_at = new Date();
     return this.userProfileRepository.save(userProfileEntity);
+  }
+
+  async updateUserProfile(
+    userProfileEntity: UserProfileEntity,
+    updateUserProfile: UpdateUserProfileDto,
+  ) {
+    const { rank, money } = updateUserProfile;
+    await this.userProfileRepository
+      .createQueryBuilder()
+      .update(userProfileEntity)
+      .set({ rank, money, updated_at: new Date() })
+      .where('user_id = :user_id', { user_id: userProfileEntity.user_id })
+      .execute();
   }
 }
