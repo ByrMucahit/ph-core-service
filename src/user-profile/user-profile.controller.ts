36ccd23311ca61a72dcfe011647278f3921db6ff
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-25 21:50:58 +0300

    feat: "update user-profile.controller.ts to add new endpoint to update user profile"

--- src/user-profile/user-profile.controller.ts
+++ src/user-profile/user-profile.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Put } from '@nestjs/common';
 import { UserProfileService } from './user-profile.service';
 import { UUIDParam } from '../decorators/http.decorators';
+import { UpdateUserProfileDto } from './exceptions/update-user-profile.dto';
 
 @Controller('user-profile')
 export class UserProfileController {
@@ -10,5 +11,14 @@ export class UserProfileController {
   @HttpCode(HttpStatus.OK)
   async createUserProfile(@UUIDParam('user_id') userId: string) {
     return this.userProfileService.createUserProfile(userId);
+  }
+
+  @Put('/:user_id')
+  @HttpCode(HttpStatus.OK)
+  async updateUserProfile(
+    @UUIDParam('user_id') userId: string,
+    @Body() updateUserProfile: UpdateUserProfileDto,
+  ) {
+    return this.userProfileService.updateUserProfile(userId, updateUserProfile);
   }
 }
