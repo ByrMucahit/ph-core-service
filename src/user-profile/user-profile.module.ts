facd06052e5860ea81681aeb9abe532727ae2e74
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-01 11:51:51 +0300

    feat: "update user-profile.module.ts to add required module"

--- src/user-profile/user-profile.module.ts
+++ src/user-profile/user-profile.module.ts
@@ -4,9 +4,11 @@ import { UserProfileEntity } from './user-profile.entity';
 import { UserProfileService } from './user-profile.service';
 import { UserProfileController } from './user-profile.controller';
 import { UserProfileDataAccess } from './user-profile.data-access';
+import { MoneyTransactionModule } from '../money-transaction/money-transaction.module';
+import { RedisModules } from '../redis/cache/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserProfileEntity])],
+  imports: [TypeOrmModule.forFeature([UserProfileEntity]), MoneyTransactionModule, RedisModules],
   providers: [UserProfileService, UserProfileDataAccess],
   controllers: [UserProfileController],
   exports: [UserProfileService],
