6cb7bc103472298b61530e803f3d192782ba8219
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-01 15:13:54 +0300

    feat: "add cache.module.ts to set up redis provider"

---
+++ src/redis/cache/cache.module.ts
@@ -0,0 +1,19 @@
+import { Module, Provider } from '@nestjs/common';
+import { Redis } from 'ioredis';
+
+export const REDIS_CLIENT = 'REDIS_CLIENT';
+
+export const redisProvider: Provider = {
+  provide: REDIS_CLIENT,
+  useFactory: () => {
+    return new Redis({
+      host: 'localhost',
+      port: 6379,
+    });
+  },
+};
+@Module({
+  providers: [redisProvider],
+  exports: [redisProvider],
+})
+export class CacheModule {}
