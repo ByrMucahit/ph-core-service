74175508d294d012fb437b3872b27754fec6edc1
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-02 11:37:16 +0300

    feat: "update app.module.ts to register redis"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { DataSourceModule } from './datasource/typeorm.module';
 import { UsersModule } from './users/users.module';
 import { AppGateway } from './gateway/app-gateway';
 import { MoneyTransactionModule } from './money-transaction/money-transaction.module';
-import { CacheModule } from './redis/cache/cache.module';
 import { UserProfileModule } from './user-profile/user-profile.module';
 import { JobModule } from './redis/bullmq/job.module';
+import { GlobalRedisModule } from './redis/global-redis.module';
+import { CacheModule } from './redis/cache/cache.module';
 
 @Module({
   imports: [
     DataSourceModule,
     UsersModule,
     MoneyTransactionModule,
+    GlobalRedisModule,
     CacheModule,
     UserProfileModule,
     JobModule,
