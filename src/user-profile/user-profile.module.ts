b51b7203e226c80c93c47dac186c1f1a4fc2ce7b
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-02 11:44:33 +0300

    feat: "update user-profile.module.ts to add cache module to user-profile.module.ts"

--- src/user-profile/user-profile.module.ts
+++ src/user-profile/user-profile.module.ts
@@ -5,9 +5,10 @@ import { UserProfileService } from './user-profile.service';
 import { UserProfileController } from './user-profile.controller';
 import { UserProfileDataAccess } from './user-profile.data-access';
 import { MoneyTransactionModule } from '../money-transaction/money-transaction.module';
+import { CacheModule } from '../redis/cache/cache.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserProfileEntity]), MoneyTransactionModule],
+  imports: [TypeOrmModule.forFeature([UserProfileEntity]), MoneyTransactionModule, CacheModule],
   providers: [UserProfileService, UserProfileDataAccess],
   controllers: [UserProfileController],
   exports: [UserProfileService],
