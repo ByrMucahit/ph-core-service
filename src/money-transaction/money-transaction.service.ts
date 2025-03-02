22d9ebb473eeeac9812e66711486c09a5d4df3bf
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-02 18:59:09 +0300

    feat: "update money-transaction.service.ts to add new methods that allow us to distribute awards between users"

--- src/money-transaction/money-transaction.service.ts
+++ src/money-transaction/money-transaction.service.ts
@@ -24,4 +24,43 @@ export class MoneyTransactionService {
   async findSumMoneyTransactionByDateAndUserId(startDate: Date, endDate: Date) {
     return this.moneyTransactionDataAccess.sumMoneyTransactionByDateAndUserId(startDate, endDate);
   }
+
+  async findSumMoneyTransactionByDateAndUserIds(startDate: Date, endDate: Date, userIds: string[]) {
+    return this.moneyTransactionDataAccess.sumMoneyTransactionByDateAndUserIds(
+      startDate,
+      endDate,
+      userIds,
+    );
+  }
+
+  async collectMoneyForAwardingByDate(startDate: Date, endDate: Date) {
+    return this.moneyTransactionDataAccess.sumMoneyInPoolForAwardingByDate(startDate, endDate);
+  }
+
+  calculateRationOnRank(totalPrize: number) {
+    const rewards: { [rank: number]: number } = {};
+    rewards[1] = 0.2 * totalPrize;
+    rewards[2] = 0.15 * totalPrize;
+    rewards[3] = 0.1 * totalPrize;
+
+    const remainingPrize = 0.55 * totalPrize;
+    let totalInverse = 0;
+    for (let k = 4; k <= 100; k++) {
+      totalInverse += 1 / k;
+    }
+    for (let rank = 4; rank <= 100; rank++) {
+      rewards[rank] = (remainingPrize / totalInverse) * (1 / rank);
+    }
+    return rewards;
+  }
+
+  async distributeAwardOnRank(userProfiles: any[], amountMoneyForAward: number) {
+    const prizeList = this.calculateRationOnRank(amountMoneyForAward);
+    const userProfileList: any = [];
+    for (let index = 0; index < userProfiles.length; index++) {
+      const haveMoney: number = userProfiles[index].money;
+      const calculatedMoney = haveMoney + prizeList[index + 1];
+      userProfileList.push({ money: calculatedMoney });
+    }
+  }
 }
