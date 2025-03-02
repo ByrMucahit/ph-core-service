5979862f8434deba0fd4484e28ab60ed09f1be29
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-02 11:39:41 +0300

    feat: "add date-range.ts to get week range"

---
+++ src/helpers/date-range.ts
@@ -0,0 +1,12 @@
+export function getWeekRange() {
+  const dayOfWeek = new Date().getDay();
+
+  const firstDay: Date = new Date();
+  firstDay.setDate(firstDay.getDate() - (dayOfWeek === 0 ? 6 : dayOfWeek - 1));
+  firstDay.setHours(0, 0, 0, 0);
+
+  const lastDay = new Date(firstDay);
+  lastDay.setDate(firstDay.getDate() + 6);
+  lastDay.setHours(23, 59, 59, 999);
+  return { firstDay, lastDay };
+}
