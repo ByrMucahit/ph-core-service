28710e74be4ca8162fba07ad3f6ac282032a1a45
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-02 18:57:32 +0300

    feat: "update job.processor.ts to distribute awards between user and reset earning money of users"

--- src/redis/bullmq/job-processor.service.ts
+++ src/redis/bullmq/job-processor.service.ts
@@ -1,12 +1,36 @@
 import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
+import { UsersService } from '../../users/users.service';
+import { MoneyTransactionService } from '../../money-transaction/money-transaction.service';
+import { CacheService } from '../cache/cache.service';
+import { UserProfileService } from '../../user-profile/user-profile.service';
 
 @Processor('weeklyJobQueue')
 export class JobProcessorService {
-  @Process()
+  constructor(
+    private usersService: UsersService,
+    private userProfileService: UserProfileService,
+    private moneyTransactionService: MoneyTransactionService,
+    private cacheService: CacheService,
+  ) {}
+
+  @Process('weekly-job')
   async handleJob(job: Job) {
     console.log('Job started: ', job.id);
-
+    const users = await this.usersService.findUsers({ id: 1 });
+    const userProfiles = await this.userProfileService.findTop100UserProfiles();
+    console.log('users: ', users, userProfiles);
+    const weekRange: any = await this.cacheService.getWeekRange();
+    const moneyTransaction = await this.moneyTransactionService.collectMoneyForAwardingByDate(
+      new Date(weekRange.start_date),
+      new Date(weekRange.end_date),
+    );
+    await this.moneyTransactionService.distributeAwardOnRank(
+      userProfiles,
+      moneyTransaction.sum_award_money,
+    );
+    console.log(moneyTransaction);
+    await this.cacheService.resetAllUserProfiles();
     console.log('Job completed: ', job.id);
   }
 }
