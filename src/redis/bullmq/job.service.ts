e746abf62f4e546ef9d1b997f20aabe4dd8d7df5
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-02 11:41:55 +0300

    feat: "update job.service.ts to extend scheduling logic"

--- src/redis/bullmq/job.service.ts
+++ src/redis/bullmq/job.service.ts
@@ -1,11 +1,11 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
-import { Redis } from 'ioredis';
-import { REDIS_CLIENT } from '../cache/cache.module';
 import { UsersEntity } from '../../users/users.entity';
 import { UsersService } from '../../users/users.service';
 import { UserProfileInCacheDto } from '../../user-profile/dtos/user-profile-in-cache.dto';
+import { CacheService } from '../cache/cache.service';
+import { getWeekRange } from '../../helpers/date-range';
 
 @Injectable()
 export class JobService {
@@ -13,7 +13,7 @@ export class JobService {
 
   constructor(
     @InjectQueue('weeklyJobQueue') private weeklyJobQueue: Queue,
-    @Inject(REDIS_CLIENT) private readonly redisClient: Redis,
+    private cacheService: CacheService,
     private userServices: UsersService,
   ) {}
 
@@ -32,39 +32,25 @@ export class JobService {
   async scheduleWeeklyJobQueue() {
     this.logger.debug('Scheduling job...');
     // await this.weeklyJobQueue.add({}, { repeat: { cron: '0 0 * * 1' } });
-    const userInCache = await this.getUserProfilesInCache();
+    const userInCache = await this.cacheService.getUserProfiles();
     if (userInCache.length < 1) {
       this.logger.debug(`There is not data in redis`);
       const users: UsersEntity[] = await this.userServices.findUsers({ id: 1 });
       const userProfileInMemoryCache = this.makeUserToUserProfile(users);
-      await this.updateMultipleUsers(userProfileInMemoryCache);
+      await this.cacheService.updateMultipleUsers(userProfileInMemoryCache);
     }
-    const jobs = await this.weeklyJobQueue.getJobs([
-      'waiting',
-      'active',
-      'delayed',
-      'completed',
-      'failed',
-    ]);
+
+    const weekRange = await this.cacheService.getWeekRange();
+    if (!weekRange) {
+      const currentWeekRange = getWeekRange();
+      await this.cacheService.setWeekRange(currentWeekRange.firstDay, currentWeekRange.lastDay);
+    }
+
+    const jobs = await this.weeklyJobQueue.getJobs(['active']);
     const existFlag = jobs.some((job) => job.name === 'weekly-job');
     if (!existFlag) {
       this.logger.debug(`Creating weekly job...`);
       await this.weeklyJobQueue.add('weekly-job', { repeat: { every: 10000 } });
     }
-  }
-
-  async getUserProfilesInCache() {
-    const users = await this.redisClient.zrevrange('ordered_user_profile_on_money', 0, -1);
-    return users.map((user) => JSON.parse(user));
-  }
-
-  async updateMultipleUsers(userProfiles: UserProfileInCacheDto[]) {
-    const pipeline = this.redisClient.pipeline();
-
-    for (const user of userProfiles) {
-      const userString = JSON.stringify(user);
-      pipeline.zadd('ordered_user_profile_on_money', user.money, userString);
-    }
-    await pipeline.exec();
   }
 }
