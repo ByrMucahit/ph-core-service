1847dcee356c1151c9a911fdfb634735ad2ca503
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-03-03 17:28:18 +0300

    feat: "update user-profile.module.ts to add global redis module"

--- src/user-profile/user-profile.module.ts
+++ src/user-profile/user-profile.module.ts
@@ -6,9 +6,15 @@ import { UserProfileController } from './user-profile.controller';
 import { UserProfileDataAccess } from './user-profile.data-access';
 import { MoneyTransactionModule } from '../money-transaction/money-transaction.module';
 import { CacheModule } from '../redis/cache/cache.module';
+import { GlobalRedisModule } from '../redis/global-redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserProfileEntity]), MoneyTransactionModule, CacheModule],
+  imports: [
+    TypeOrmModule.forFeature([UserProfileEntity]),
+    MoneyTransactionModule,
+    CacheModule,
+    GlobalRedisModule,
+  ],
   providers: [UserProfileService, UserProfileDataAccess],
   controllers: [UserProfileController],
   exports: [UserProfileService],
